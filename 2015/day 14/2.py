d80c4dfbcb6f3c9e6e041a9a763c71bb1cc68053
Barep Bimo Pangestu <bimo.pangestu09@gmail.com>
2022-04-06 15:04:48 +0700

    Update 2.py

--- 2015/day 14/2.py
+++ 2015/day 14/2.py
@@ -9,18 +9,9 @@ rest = []
 flight = []
 point = []
 distance = []
+flight_status = []
+phase_time = []
 TIME = 2503
-
-# def find_status(time):
-#     for i in range(len(deers)):
-#         if 
-
-def calc_distance(time):
-    for i in range(len(deers)):
-        if ((time+1)%(flight[i]+rest[i]) > flight[i]):
-            distance[i] +=0
-        else:
-            distance[i] += speed[i]
 
 for i in range(len(data)):
     line = data[i].strip().split(' ')
@@ -29,17 +20,43 @@ for i in range(len(data)):
     speed.append(int(line[1]))
     flight.append(int(line[2]))
     rest.append(int(line[3]))
-    # flight.append(True)
+    flight_status.append(True)
     distance.append(0)
+    phase_time.append(0)
 
-calc_distance(135)
+def find_status():
+    for deer in range(len(deers)):
+        if (flight_status[deer] == True):
+            if (phase_time[i] > flight[i]):
+                phase_time[i] = 0
+                flight_status[i] = False
+            else:
+                phase_time +=1
+        else:
+            if (phase_time[i] > rest[i]):
+                phase_time[i] = 0
+                flight_status[i] = True
+            else:
+                phase_time +=1
 
-# for i in range(TIME):
-#     calc_distance(i)
+def calc_distance(time):
+    for i in range(len(deers)):
+        if flight_status[i] == True:
+            distance[i] += speed[i]
+        else:
+            distance[i] += 0
 
 
+# calc_distance(6)
 print (deers)
 print (speed)
 print (flight)
 print (rest)
+print (phase_time)
+
+for i in range(TIME):
+    find_status()
+    calc_distance(i+1)
+
+
 print(distance)

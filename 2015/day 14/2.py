6d0f12c5aca3a408c161a4549f0aaf1602f9885f
Barep Bimo Pangestu <bimo.pangestu09@gmail.com>
2022-04-06 19:18:59 +0700

    day 14 problem 2 solved

--- 2015/day 14/2.py
+++ 2015/day 14/2.py
@@ -1,6 +1,9 @@
+import numpy as np
+
 file = open('input2.txt')
 data = file.readlines()
 file.close()
+# outfile = open('exout.txt', 'w')
 
 deers = []
 speed = []
@@ -23,40 +26,49 @@ for i in range(len(data)):
     flight_status.append(True)
     distance.append(0)
     phase_time.append(0)
+    point.append(0)
 
 def find_status():
-    for deer in range(len(deers)):
-        if (flight_status[deer] == True):
-            if (phase_time[i] > flight[i]):
-                phase_time[i] = 0
+    for i in range(len(deers)):
+        if (flight_status[i] == True):
+            if (phase_time[i] > flight[i] - 1):
+                # print(phase_time[i])
+                phase_time[i] = 1
                 flight_status[i] = False
             else:
-                phase_time +=1
+                phase_time[i] +=1
         else:
-            if (phase_time[i] > rest[i]):
-                phase_time[i] = 0
+            if (phase_time[i] > rest[i] - 1):
+                phase_time[i] = 1
                 flight_status[i] = True
             else:
-                phase_time +=1
+                phase_time[i] +=1
 
-def calc_distance(time):
+def calc_distance():
     for i in range(len(deers)):
         if flight_status[i] == True:
             distance[i] += speed[i]
         else:
             distance[i] += 0
 
+def addPoint():
+    front = max(distance)
+    # print(front)
+    leaders = np.where(np.array(distance) == front)[0]
+    for i in leaders:
+        point[i] += 1
 
 # calc_distance(6)
-print (deers)
-print (speed)
-print (flight)
-print (rest)
-print (phase_time)
+# print (deers)
+# print (speed)
+# print (flight)
+# print (rest)
 
 for i in range(TIME):
     find_status()
-    calc_distance(i+1)
+    calc_distance()
+    addPoint()
+    # outfile.write('second: ' + str(i+1) + '\n' + deers[0] + ': ' + str(distance[0]) + ', ' + str(flight_status[0]) + '\n' + deers[1] + ': ' + str(distance[1]) + ', ' + str(flight_status[1]) + '\n')
 
-
-print(distance)
+# outfile.close()
+print(point)

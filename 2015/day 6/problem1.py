35590fe9defe3c5b99924eccb8b2945b4182a94a
Barep Bimo Pangestu <bimo.pangestu09@gmail.com>
2023-04-20 06:57:57 +0700

    use class

--- 2015/day 6/problem1.py
+++ 2015/day 6/problem1.py
@@ -1,34 +1,4 @@
-from typing import NamedTuple, TypeVar
-
-T = TypeVar('T')
-array = list[list[bool]]
-
-
-class InstructionMap(NamedTuple):
-    x1: int
-    y1: int
-    x2: int
-    y2: int
-
-
-def run_instruction(
-    grid: array, instruction: str, map: InstructionMap
-) -> array:
-    match instruction:
-        case 'toggle':
-            operation = toggle
-        case 'on':
-            operation = turn_on
-        case 'off':
-            operation = turn_off
-        case _:
-            raise NotImplementedError()
-    # print(grid)
-    for x in range(map.x1, map.x2 + 1):
-        for y in range(map.y1, map.y2 + 1):
-            grid[x][y] = operation(grid[x][y])
-    # print(grid)
-    return grid
+from common import parse_command, Operator
 
 
 def toggle(node: bool) -> bool:
@@ -43,27 +13,20 @@ def turn_off(node: bool) -> bool:
     return not (node or True)
 
 
-def parse_command(input: str) -> tuple[str, InstructionMap]:
-    command = input.lower().split(' ')
-    if 'turn' in command:
-        command.remove('turn')
-    command.remove('through')
-    instruction = command[0]
-    x1, y1 = command[1].split(',')
-    x2, y2 = command[2].split(',')
-    return instruction, InstructionMap(int(x1), int(y1), int(x2), int(y2))
-
-
 def main() -> None:
     grid = [[False] * 1000 for _ in range(1000)]
+    operator = Operator(
+        grid,
+        {'toggle': toggle, 'turn_on': turn_on, 'turn_off': turn_off}
+    )
 
     with open('input.txt') as f:
         for string in f.read().splitlines():
             instruction, map = parse_command(string)
-            grid = run_instruction(grid, instruction, map)
+            operator.run_instruction(instruction, map)
 
-    active_lights = 0
-    for x in grid:
+    active_lights: int = 0
+    for x in operator.grid:
         for y in x:
             active_lights += y
 

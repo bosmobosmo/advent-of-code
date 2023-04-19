067ae195085742fa909d47f1e55cec2e51d27977
Barep Bimo Pangestu <bimo.pangestu09@gmail.com>
2023-04-20 00:08:10 +0700

    improve code for 2015 day 5 problem 1

---
+++ 2015/day 5/problem1.py
@@ -0,0 +1,38 @@
+VOWELS = 'aeiou'
+
+with open('input.txt') as f:
+    input_string = f.read()
+
+
+def is_nice(string: str) -> bool:
+    return has_three_vowels(string) \
+        and has_repeats(string) \
+        and not has_special_substring(string)
+
+
+def has_three_vowels(string: str) -> bool:
+    vowels = 0
+    for vowel in VOWELS:
+        vowels += string.count(vowel)
+    return vowels >= 3
+
+
+def has_repeats(string: str) -> bool:
+    for i in range(len(string) - 1):
+        if string[i] == string[i+1]:
+            return True
+    return False
+
+
+def has_special_substring(string: str) -> bool:
+    return 'ab' in string \
+        or 'cd' in string \
+        or 'pq' in string \
+        or 'xy' in string
+
+
+nice_strings = 0
+for string in input_string.splitlines():
+    if is_nice(string):
+        nice_strings += 1
+print(f"Number of nice string is {nice_strings}")

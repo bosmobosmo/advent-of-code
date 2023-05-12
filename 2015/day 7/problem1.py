08dd5a82d00e35da32dd46dc50a143ad3c28815b
Barep Bimo Pangestu <bimo.pangestu09@gmail.com>
2023-05-12 23:12:19 +0700

    improve code for 2015 day 7 problem 1

--- 2015/day 7/problem1.py
+++ 2015/day 7/problem1.py
@@ -1,96 +1,101 @@
-f=open("input.txt")
-dat=f.read()
-f.close()
+from __future__ import annotations
+from dataclasses import dataclass
+from typing import Callable, Sequence, Union
 
-lines=dat.splitlines()
-wires = {}
+InputType = Sequence[str]
+LogicGate = Callable[[InputType], int]
+CIRCUIT: dict[str, Union[Command, int]] = {}
 
 
+@dataclass
+class Command:
+    gate: LogicGate
+    inputs: InputType
 
-while (len(lines)>0):
-    print len(lines)
-    # print wires
-    for i in lines[:]:
-        s = i.split()
-        if len(s) == 3:
-            # print "masuk s3"
-            if s[0].isdigit():
-                # print "masuk"
-                # print s
-                wires[s[2]] = int(s[0]) & 65535
 
-                #problem 2
-                if s[2] == 'b':
-                    wires['b'] = 3176
+def bitwise_and(inputs: InputType) -> int:
+    this_input = [
+        int(input)
+        if input.isdigit()
+        else get_output(input)
+        for input in inputs
+    ]
+    return this_input[0] & this_input[1]
 
-                # print wires
-                lines.remove(i)
-            elif s[0] in wires:
-                print i
-                # print s
-                wires[s[2]] = wires[s[0]] & 65535
-                # print wires
-                lines.remove(i)
-        elif len(s) == 4:
-            # print "masuk s4"
-            if s[1] in wires:
-                # print "masuk"  
-                # print s
-                wires[s[3]] = ~wires[s[1]] & 65535
-                # print wires
-                lines.remove(i)
-        elif len(s) == 5:
-                # print "masuk sh
-            if s[1] == 'RSHIFT':
-                if s[0] in wires:
-                    # print s
-                    if s[0] == 'x':
-                        print i
-                    wires[s[4]] = wires[s[0]] >> int(s[2]) & 65535
-                    # print wires
-                    lines.remove(i)
-            elif s[1] == 'LSHIFT':
-                if s[0] in wires:
-                    # print s
-                    wires[s[4]] = wires[s[0]] << int(s[2]) & 65535
-                    # print wires
-                    lines.remove(i)
-            elif s[1] == 'OR':
-                if (s[0] in wires and s[2] in wires):
-                    # print s
-                    wires[s[4]] = wires[s[0]] | wires[s[2]] & 65535
-                    # print wires
-                    lines.remove(i)
-            elif s[1] == 'AND':
-                if s[0].isdigit() and s[2] in wires:
-                    # print s
-                    wires[s[4]] = int(s[0]) & wires[s[2]] & 65535
-                    # print wires
-                    lines.remove(i)
-                elif (s[0] in wires and s[2] in wires):
-                    # print s
-                    wires[s[4]] = wires[s[0]] & wires[s[2]] & 65535
-                    # print wires
-                    lines.remove(i)
 
-# keylist = wires.keys()
-# keylist.sort()
-# for key in keylist:
-#     print "%s : %s" % (key, wires[key])
+def bitwise_or(inputs: InputType) -> int:
+    this_input = [
+        int(input)
+        if input.isdigit()
+        else get_output(input)
+        for input in inputs
+    ]
+    return this_input[0] | this_input[1]
 
-# wires["dd"] = 40
-# wires["bb"] = 50
 
-# if ("bb" and "dd") in wires:
-#     print wires["bb"] + wires["dd"]
+def bitwise_not(inputs: InputType) -> int:
+    input = get_output(inputs[0])
+    return ~input
 
-print wires['a']
-# for i in lines:
-#     s = i.split()
-#     for e in s:
-#         if e == 'a':
-#             print i
-#             continue
-#     if len(s) == 5:
-#         if s[1] == 'LSHIFT':
-#             print s
+
+def bitwise_lshift(inputs: InputType) -> int:
+    steps = int(inputs[1])
+    input = get_output(inputs[0])
+    return input << steps
+
+
+def bitwise_rshift(inputs: InputType) -> int:
+    steps = int(inputs[1])
+    input = get_output(inputs[0])
+    return input >> steps
+
+
+def direct_assignment(inputs: InputType) -> int:
+    if inputs[0].isdigit():
+        input = int(inputs[0])
+    else:
+        input = get_output(inputs[0])
+    return input
+
+
+def get_output(input: str) -> int:
+    stored_value = CIRCUIT.get(input)
+    if stored_value is None:
+        raise Exception("Command not found")
+    if isinstance(stored_value, int):
+        return stored_value
+    output = stored_value.gate(stored_value.inputs)
+    CIRCUIT[input] = output
+    return output
+
+
+def main() -> None:
+    with open('input.txt') as f:
+        for instruction in f.read().splitlines():
+            command, output = _parse_instruction(instruction)
+            CIRCUIT[output] = command
+    print(get_output('a'))
+
+
+def _parse_instruction(instruction: str) -> tuple[Command, str]:
+    words = instruction.split(" ")
+    if len(words) == 3:
+        return Command(direct_assignment, [words[0]]), words[-1]
+    elif len(words) == 4:
+        return Command(bitwise_not, [words[1]]), words[-1]
+    inputs = [words[0], words[2]]
+    match words[1]:
+        case "AND":
+            return Command(bitwise_and, inputs), words[-1]
+        case "OR":
+            return Command(bitwise_or, inputs), words[-1]
+        case "LSHIFT":
+            return Command(bitwise_lshift, inputs), words[-1]
+        case "RSHIFT":
+            return Command(bitwise_rshift, inputs), words[-1]
+        case _:
+            print('Invalid instruction')
+    raise Exception("Invalid instruction")
+
+
+main()
